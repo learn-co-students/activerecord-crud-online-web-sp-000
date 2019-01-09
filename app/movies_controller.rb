f957ea892c0acfe1a2a36dc9b1f3203c44ad661a
Cookie McCormick <muntananuchat@gmail.com>
2019-01-09 16:08:29 -0500

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -12,7 +12,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.create(attributes)
+  Movie.create(attributes)
 end
 
 def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
@@ -51,7 +51,6 @@ def can_be_found_updated_and_saved
   movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
   movie.save
-  movie
 end
 
 def can_update_using_update_method
