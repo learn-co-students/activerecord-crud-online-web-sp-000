f4c0be00554a708fccb70e2acf7e1fc7ca8e4aa4
Lukie Kang <Lukie Kang>
2019-10-08 21:43:55 +0100

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_07_170200) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
