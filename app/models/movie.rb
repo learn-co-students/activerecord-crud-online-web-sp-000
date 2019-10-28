451efe3bf6040278474589cda8ba4ac330fc7ce4
Bernadette Davis <bsd231@nyu.edu>
2019-10-28 18:04:53 +0000

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,3 @@
 class Movie < ActiveRecord::Base
-=begin
-  def initialize(attr = {})
-    @title = attr[title:]
-    @release_date = attr[:release_date]
-    @director = attr[:director]
-    @lead = attr[:lead]
-    @in_theaters = attr[:in_theaters]
 
-  end
-=end
 end
