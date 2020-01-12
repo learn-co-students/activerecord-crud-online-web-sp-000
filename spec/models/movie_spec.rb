9e0702b5179a43a947116acd1fcb64718a172213
Anthony Tilelli <anthony@tilelli.me>
2020-01-12 06:03:28 +0000

    Done.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -35,7 +35,7 @@ describe 'Movie' do
 
     it 'has an in theaters flag' do
       movie.in_theaters = false
-      binding.pry
+      # binding.pry
       expect(movie.in_theaters?).to be_falsey
     end
   end
