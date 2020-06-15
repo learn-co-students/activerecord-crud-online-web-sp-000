cf4d1e043d2c9ccd942d65c17dc6b50ea2d1b3db
mbade1 <mbade314@gmail.com>
2020-06-15 17:24:12 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_06_15_211656) do
 
-  create_table "create_movies", force: :cascade do |t|
+  create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_date"
     t.string "director"
     t.string "lead"
     t.boolean "in_theaters"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
