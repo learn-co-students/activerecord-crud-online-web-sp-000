c230ec43c9a8d9579214bfaf7fc0f697c66b6d30
AmyFong82 <amyhfong@gmail.com>
2020-07-21 01:15:52 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,19 +35,19 @@ def can_be_created_in_a_block(args = {title:"Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  movie = Movie.first
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  movie = Movie.last
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  size = Movie.count
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  movie = Movie.find_by(id: 1)
+  Movie.find_by(id:1)
 end
 
 def can_find_by_multiple_attributes
@@ -55,7 +55,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  movie = Movie.where(title: "Title", release_date: 2000, director: "Me")[0]
+  movie = Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
