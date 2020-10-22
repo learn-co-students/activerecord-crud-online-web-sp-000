c24b63cc6d1bd34bd7fabb51b7d11525adc2b804
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2020-10-22 18:51:19 -0500

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -62,7 +62,8 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  #Movie.where(release_date: > 2002)
+  m = Movie.where("release_date > 2002")
+  m.order(release_date: :desc) 
 end
 
 def can_be_found_updated_and_saved
@@ -75,7 +76,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
+  
   __
 end
 
