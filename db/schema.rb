17407266e23a59cd9789ff16ff26e43f5865dd30
Catherine Gloria <catherinegloria@gmail.com>
2020-12-11 13:40:28 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_12_11_152545) do
 
-  create_table "movie", force: :cascade do |t|
+  create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_date"
     t.string "director"
